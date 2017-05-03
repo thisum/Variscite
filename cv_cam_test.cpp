7f72d63887c3d39e87c7d5d353a12044071e1abc
Thisum Buddhika <thisum.kb@gmail.com>
2017-05-03 16:25:06 +0800

    integrate image processing

--- cv_cam_test.cpp
+++ cv_cam_test.cpp
@@ -4,6 +4,14 @@
 
 using namespace cv;
 
+struct ROW
+{
+    int minx = 10000;
+    int miny = 10000;
+    int maxx = 0;
+    int maxy = 0;
+};
+
 int takeImage(int argc, char** argv )
 {
     
@@ -85,17 +93,187 @@ int takeImage(int argc, char** argv )
 
 int processImage()
 {
-    Mat image;
-    image = imread( "test.jpeg", 1 );
+    Mat image = imread( "test.jpeg", 1 );
 
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
+    int elementSize = 3;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<std::vector<cv::Point> > contours2;
+    std::vector<std::vector<cv::Point> > contours3;
+    if ( !image.data )
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    int cols = image.cols;
+    int rows = image.rows;
+    Mat M = getRotationMatrix2D(Point(cols/2, rows/2), -90, 1);
+    Mat frame = Mat::zeros( rows, cols, image.type() );
+    Mat frame2 = Mat::zeros( rows, cols, image.type() );
+    Mat frame3 = Mat::zeros( rows, cols, image.type() );
+    warpAffine(image, frame, M, frame.size());
+    Mat original = frame.clone();
+
+    cvtColor(frame, frame, COLOR_BGR2HSV);
+    inRange(frame, Scalar(90, 100, 30), Scalar(130, 255, 255), frame);
+    medianBlur(frame, frame, 3);
+    Mat element = getStructuringElement(MORPH_ELLIPSE, Size( 2*elementSize + 1, 2*elementSize+1 ), Point(elementSize, elementSize));
+    dilate(frame, frame, element);
+    findContours(frame, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    if(!contours.empty())
+    {
+        int i = 0;
+        while(i<contours.size())
+        {
+            if( contourArea(contours[i]) < (cols * rows / 1000) )
+            {
+                contours.erase(contours.begin()+i);
+            }
+            else
+            {
+                i += 1;
+            }
+        }
+
+        std::vector<ROW> data;
+        for(i=0;i<contours.size();i++)
+        {
+            ROW newRow;
+            data.push_back(newRow);
+            for(int j =0; j< contours[i].size(); j++)
+            {
+                Point p = contours[i][j];
+                if(p.x < newRow.minx)
+                {
+                    newRow.minx = p.x;
+                }
+                if(p.x > newRow.maxx)
+                {
+                    newRow.maxx = p.x;
+                }
+                if(p.y < newRow.miny)
+                {
+                    newRow.miny = p.y;
+                }
+                if(p.y > newRow.maxy)
+                {
+                    newRow.maxy = p.y;
+                }
+            }
+        }
+
+        cvtColor(original, frame2, COLOR_BGR2HSV);
+        inRange(frame2, Scalar(140, 70, 100), Scalar(179, 255, 255), frame2);
+        medianBlur(frame2, frame2, 3);
+        dilate(frame2, frame2, element);
+        findContours(frame2, contours2, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        std::vector<ROW> data2;
+        for(i=0;i<contours2.size();i++)
+        {
+            ROW newRow;
+            data2.push_back(newRow);
+            for(int j =0; j< contours2[i].size(); j++)
+            {
+                Point p = contours2[i][j];
+                if(p.x < newRow.minx)
+                {
+                    newRow.minx = p.x;
+                }
+                if(p.x > newRow.maxx)
+                {
+                    newRow.maxx = p.x;
+                }
+                if(p.y < newRow.miny)
+                {
+                    newRow.miny = p.y;
+                }
+                if(p.y > newRow.maxy)
+                {
+                    newRow.maxy = p.y;
+                }
+            }
+        }
+
+        cvtColor(original, frame3, COLOR_BGR2HSV);
+        inRange(frame3, Scalar(0, 100, 100), Scalar(10, 255, 255), frame3);
+        medianBlur(frame3, frame3, 3);
+        dilate(frame3, frame3, element);
+        findContours(frame3, contours3, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        std::vector<ROW> data3;
+        for(i=0;i<contours3.size();i++)
+        {
+            ROW newRow;
+            data3.push_back(newRow);
+            for(int j =0; j< contours3[i].size(); j++)
+            {
+                Point p = contours3[i][j];
+                if(p.x < newRow.minx)
+                {
+                    newRow.minx = p.x;
+                }
+                if(p.x > newRow.maxx)
+                {
+                    newRow.maxx = p.x;
+                }
+                if(p.y < newRow.miny)
+                {
+                    newRow.miny = p.y;
+                }
+                if(p.y > newRow.maxy)
+                {
+                    newRow.maxy = p.y;
+                }
+            }
+        }
+
+        i = 0;
+        while( i<contours.size())
+        {
+            bool f = false;
+            for(int j=0; j<data2.size(); j++)
+            {
+                if( ( (data[i].minx <= data2[j].minx) and (data[i].maxx >= data2[j].maxx)
+                        and (data[i].miny <= data2[j].miny) and (data[i].maxy >= data2[j].maxy))
+                    or ( (data[i].minx <= data3[j].minx) and (data[i].maxx >= data3[j].maxx)
+                         and (data[i].miny <= data3[j].miny) and data[i].maxy >= data3[j].maxy))
+                f = true;
+                break;
+            }
+            if(f)
+            {
+                i += 1;
+            }
+            else
+            {
+                contours.erase(contours.begin() + i);
+                data.erase(data.begin() + i);
+            }
+
+        }
+
+        if (contours.size()==1)
+        {
+            printf("Ring Found: \n");
+            printf("Location: Top Left Corner: (%d, %d), Bottom Right Corner: (%d, %d)", data[0].minx, data[0].miny, data[0].maxx, data[0].maxy);
+        }
+        else if( contours.empty() )
+        {
+            printf("No Ring Found");
+        }
+        else
+        {
+            printf("Multiple Rings Found");
+        }
+
+    }
     else
     {
-	printf("image can be read\n");
+        printf("No Rings Found");
     }
 
     return 0;
