c4bb7545b02d4dda007f9acdf33b4f18897b08cb
Thisum <thisum@ahlab.org>
2017-05-05 16:31:57 +0800

    video

--- video_capture.cpp
+++ video_capture.cpp
@@ -3,17 +3,41 @@
 //
 #include <opencv2/opencv.hpp>
 #include <gst/gst.h>
+#include <stdio.h>
+#include <opencv2/highgui/highgui.hpp>
 #include "video_capture.h"
 
 using namespace cv;
 
 void captureImageFromVideo()
 {
-    VideoCapture cap("imxv4l2videosrc device=\"/dev/video0\" ! autovideosink ");
+    cv::VideoCapture cap("v4l2src ! video/x-raw, framerate=30/1, width=640, height=480, format=RGB ! videoconvert ! appsink");
+    if (!cap.isOpened()) {
+        printf("=ERR= can't create video capture\n");
+        return;
+    }
 
-    if(!cap.isOpened())  // check if we succeeded
-        printf("camera cannot be opened \n");
+    // second part of sender pipeline
+    cv::VideoWriter writer;
+    writer.open("appsrc ! videoconvert ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=4 ! mpegtsmux ! udpsink host=localhost port=9999"
+            , 0, (double)30, cv::Size(640, 480), true);
+    if (!writer.isOpened()) {
+        printf("=ERR= can't create video writer\n");
+        return;
+    }
 
-    else
-        printf("camera opened");
+    cv::Mat frame;
+    int key;
+
+    while (true) {
+
+        cap >> frame;
+        if (frame.empty())
+            break;
+
+        imwrite("test_1.jpeg", frame);
+
+        writer << frame;
+        key = cv::waitKey( 30 );
+    }
 }
